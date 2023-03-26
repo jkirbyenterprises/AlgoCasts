ce3157eb32a4f9e25513e945a1925e994029117c
Kirby <jkirby3286@gmail.com>
2023-03-26 12:32:32 -0400

    Working solution for maxchars, still WIP on videos

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,35 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const occurrences = {};
+    for(const char of str) {
+        occurrences[char] = occurrences[char] + 1 || 1;
+    }
+    
+    return Object.keys(occurrences).sort((a,b)=>(occurrences[b]-occurrences[a]))[0];
+}
 
 module.exports = maxChar;
+
+//First attempt (fails on numbers)
+// const occurrences = [];
+// str.split('').forEach(element => {
+//     (occurrences[element])
+//     ? occurrences[element]++
+//     : occurrences[element] = 1;
+// });
+// return Object.keys(
+//     occurrences.sort((a,b)=>b-a)
+// )[0];
+
+//Second attempt (working) with switching to object to allow numeric keys and using 
+//sort solution from https://stackoverflow.com/a/16794116
+// const occurrences = {};
+// str.split('').forEach(element => {
+//     (occurrences[element])
+//     ? occurrences[element]++
+//     : occurrences[element] = 1;
+// });
+
+// return Object.keys(occurrences).sort((a,b)=>(occurrences[b]-occurrences[a]))[0];
