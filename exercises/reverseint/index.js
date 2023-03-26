9cf9113e66a8fdb6789ba89ef3f1b8234b94c700
Kirby <jkirby3286@gmail.com>
2023-03-26 11:43:51 -0400

    Reverse Int complete

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,48 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reversed = Math.abs(n)
+    .toString()
+    .split('')
+    .reduce((rev,val) => (val+rev),'');
+    
+    return parseInt(reversed*Math.sign(n));
+}
 
 module.exports = reverseInt;
+
+//My initial solution
+// const absValue = Math.abs(n);
+// const revInt = parseInt(
+//     (absValue).toString().split('').reduce((rev, val) => (val+rev)))
+// ;
+// return (n < 0) ? -revInt : revInt;
+
+//Cleaner formatting based on grider stuff (but used reverse function)
+// const reversed = Math.abs(n)
+//     .toString()
+//     .split('')
+//     .reverse()
+//     .join('');
+    
+// return parseInt(
+//     (n < 0) ? -reversed : reversed
+// );
+
+//Using Math.sign to remove comparision
+// const reversed = Math.abs(n)
+//     .toString()
+//     .split('')
+//     .reverse()
+//     .join('');
+    
+// return parseInt(reversed*Math.sign(n));
+
+//with reduce
+// const reversed = Math.abs(n)
+// .toString()
+// .split('')
+// .reduce((rev,val) => (val+rev),'');
+
+// return parseInt(reversed*Math.sign(n));
