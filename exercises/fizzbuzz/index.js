caf43c57007aba938faa77f698c35f1896ae969a
Kirby <jkirby3286@gmail.com>
2023-03-27 19:47:34 -0400

    fizzbuzz solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,37 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    let i = 0;
+    while(++i<=n) {
+        const mulOf3 = (i % 3 === 0);
+        const mulOf5 = (i % 5 === 0);
+        if(mulOf3 && mulOf5) {
+            console.log("fizzbuzz");
+        } else if(mulOf3) {
+            console.log("fizz");
+        } else if(mulOf5) {
+            console.log("buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 module.exports = fizzBuzz;
+
+// My initial solution
+// let i = 0;
+// while(++i<=n) {
+//     const mulOf3 = (i % 3 === 0);
+//     const mulOf5 = (i % 5 === 0);
+//     if(mulOf3 && mulOf5) {
+//         console.log("fizzbuzz");
+//     } else if(mulOf3) {
+//         console.log("fizz");
+//     } else if(mulOf5) {
+//         console.log("buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
