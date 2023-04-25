e082d76a36edea67013fb6299a263cc6f8e2b112
Kirby <jkirby3286@gmail.com>
2023-04-25 19:54:13 -0400

    Capitalize solutions

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,43 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+return str.split(' ')
+            .map((part) => (
+                part[0].toUpperCase() + part.slice(1))
+            )
+            .join(' ');
+}
+
+// my initial solution
+// return str.split(' ')
+//             .map((part) => (
+//                 part[0].toUpperCase() + part.slice(1))
+//             )
+//             .join(' ');
+
+// grider inspired solution
+// let prevChar = ' ';
+// let result = '';
+// for(const char of str) {
+//     result += (prevChar === ' ')
+//                 ? char.toUpperCase()
+//                 : char;
+//     prevChar = char;
+// }
+// return result;
+
+// grider actual solution
+// let result = str[0].toUpperCase();
+
+// for(let i = 1; i < str.length; i++) {
+//     if(str[i - 1] === ' ') {
+//         result += str[i].toUpperCase();
+//     } else {
+//         result += str[i];
+//     }
+// }
+
+// return result;
 
 module.exports = capitalize;
