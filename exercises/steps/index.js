b7ea39d6b96f98638eb4e82749430119b1747ad8
Kirby <jkirby3286@gmail.com>
2023-04-25 21:08:56 -0400

    Steps solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,59 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    let suffix = ' '.repeat(n-1);
+    // let suffix = new Array(n).join(' '); //more compatible
+    let prefix = '#';
+    for(let i = 0; i < n; i++) {
+        console.log(prefix+suffix);
+        suffix = suffix.slice(1);
+        prefix += '#';
+    }
+}
+
+// my inital solution
+// help from https://stackoverflow.com/questions/14343844/create-a-string-of-variable-length-filled-with-a-repeated-character
+// on filling a string with a character
+// let suffix = ' '.repeat(n-1);
+// let suffix = new Array(n).join(' '); //more compatible
+// let prefix = '#';
+// for(let i = 0; i < n; i++) {
+//     console.log(prefix+suffix);
+//     suffix = suffix.slice(1);
+//     prefix += '#';
+// }
+
+// grider initial solution
+// for(let row = 0; row < n; row++) {
+//     let stair = '';
+//     for(let column = 0; column < n; column++) {
+//         if(column <= row) {
+//             stair += '#';
+//         } else {
+//             stair += ' ';
+//         }
+//     }
+//     console.log(stair);
+// }
+
+// grider weird recursive solution
+// if(n === row) {
+//     return;
+// }
+
+// if(n === stair.length) {
+//     console.log(stair);
+//     steps(n, row+1);
+//     return;
+// }
+
+// if(stair.length <= row) {
+//     stair += '#';
+// } else {
+//     stair += ' ';
+// }
+
+// steps(n, row, stair);
 
 module.exports = steps;
