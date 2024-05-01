0f331750a539d56778260270bf10dc93365233b8
Kirby <jkirby3286@gmail.com>
2024-04-30 21:52:18 -0400

    Matrix grider solution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,55 +15,104 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
+function matrix(n) {
+    let counter = 1;
+    let sprialMatrix = [];
+
+    for(let i = 0; i < n; i++) {
+         sprialMatrix.push([]);
+    }
+
+    let startRow = 0, startColumn = 0;
+    let endRow = n-1, endColumn = n-1;
+    
+    while(startColumn <= endColumn && startRow <= endRow) {
+        // sprialMatrix[startRow] = [];
+        for(let i=startColumn; i <= endColumn; i++) {
+            sprialMatrix[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+        for(let i=startRow; i <= endRow; i++) {
+            sprialMatrix[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+        for(let i=endColumn; i >= startColumn; i--) {
+            sprialMatrix[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+        for(let i=endRow; i >= startRow; i--) {
+            sprialMatrix[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
+    }
+    return sprialMatrix;
+}
+
+// Trying to speed it up (still 70% slower)
 // function matrix(n) {
-    // let counter = 1;
-    // let sprialMatrix = [];
-    // let startRow, startColumn = 0;
-    // let endRow, endColumn = n-1;
-    // while(startColumn <= endColumn && startRow <= endRow) {
-    //     sprialMatrix[startRow] = [];
-    //     while(startColumn++ <= endColumn) {
-    //         sprialMatrix[startRow][startColumn] = counter++;
-    //     }
-    //     startRow++;
-    //     while(startRow++ < endRow) {
-    //         sprialMatrix[startRow][endColumn] = counter++;
-    //     }
-    //     endRow--;
-    // }
+//     let xLoc = 0, yLoc = 0;
+//     let xDir = 1, yDir = 0;
+//     let step = 0;
+//     let nextX = xLoc, nextY = yLoc;
+    
+//     let spiralMatrix = [];
+//     for(let i = 0; i < n; i++) {
+//         spiralMatrix.push([]);
+//     }
+
+//     const totalLoops = (n*n) + 1;
+//     while(++step < totalLoops) {
+//         spiralMatrix[yLoc][xLoc] = step;
+
+//         // rotate 90 degrees if go outside array bounds or hit a non-undefined value
+//         nextX = xLoc + xDir, nextY = yLoc - yDir;
+
+//         if(nextX >= n || nextX < 0 || nextY >= n || nextY < 0 || spiralMatrix[nextY][nextX] != undefined) {
+//             [xDir, yDir] = [yDir, -xDir];
+//         }
+
+//         xLoc += xDir;
+//         yLoc -= yDir;
+//     }
+
+//     return spiralMatrix;
 // }
 
 // Cleaned up less-less-crappy solution
-function matrix(n) {
-    const location = { x: 0, y: 0 }
-    let direction = { x: 1, y: 0}
-    let step = 0;
+// function matrix(n) {
+//     const location = { x: 0, y: 0 }
+//     let direction = { x: 1, y: 0}
+//     let step = 0;
     
-    // (better?) way to create the base arrays (from https://stackoverflow.com/questions/53992415/how-to-fill-multidimensional-array-in-javascript)
-    const spiralMatrix = Array.from({length: n}, _=> new Array(n).fill(0));
+//     // (better?) way to create the base arrays (from https://stackoverflow.com/questions/53992415/how-to-fill-multidimensional-array-in-javascript)
+//     const spiralMatrix = Array.from({length: n}, _=> new Array(n).fill(0));
 
-    while(++step < (n*n) + 1) {
-        spiralMatrix[location.y][location.x] = step;
+//     while(++step < (n*n) + 1) {
+//         spiralMatrix[location.y][location.x] = step;
 
-        // rotate 90 degrees if go outside array bounds or hit a non-zero value
-        const nextLocation = {
-            x: location.x + direction.x,
-            y: location.y - direction.y
-        }
+//         // rotate 90 degrees if go outside array bounds or hit a non-zero value
+//         const nextLocation = {
+//             x: location.x + direction.x,
+//             y: location.y - direction.y
+//         }
 
-        if(nextLocation.x >= n || nextLocation.x < 0 || nextLocation.y >= n || nextLocation.y < 0 || spiralMatrix[nextLocation.y][nextLocation.x] != 0) {
-            direction = {
-                x: direction.y,
-                y: -direction.x
-            }
-        }
+//         if(nextLocation.x >= n || nextLocation.x < 0 || nextLocation.y >= n || nextLocation.y < 0 || spiralMatrix[nextLocation.y][nextLocation.x] != 0) {
+//             direction = {
+//                 x: direction.y,
+//                 y: -direction.x
+//             }
+//         }
 
-        location.x += direction.x;
-        location.y -= direction.y;
-    }
+//         location.x += direction.x;
+//         location.y -= direction.y;
+//     }
 
-    return spiralMatrix;
-}
+//     return spiralMatrix;
+// }
 
 // Cleaned up less-crappy solution
 // function matrix(n) {
