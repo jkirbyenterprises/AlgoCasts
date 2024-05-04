216736af612eafb284882d653b4093c4585f43ef
Kirby <jkirby3286@gmail.com>
2024-05-04 12:42:38 -0400

    Linked List Midpoint Solutions

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -12,6 +12,39 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while(fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
+// Based on Grider explaination
+// function midpoint(list) {
+//     let slowIndex = 0, fastIndex = 0;
+//     let slow = list.getAt(0);
+//     let fast = list.getAt(0);
+
+//     while(fast && fast.next && fast.next.next) {
+//         slowIndex++;
+//         fastIndex+=2;
+        
+//         slow = list.getAt(slowIndex);
+//         fast = list.getAt(fastIndex);
+//     }
+//     return slow;
+// }
+
+// My first solution (cheated and used size)
+// function midpoint(list) {
+//     const size = list.size() - 1;
+//     const midpoint = Math.floor(size / 2);
+//     return list.getAt(midpoint);
+// }
 
 module.exports = midpoint;
