98c0d0a300a06a1b1cf37d398c53c7774052fe6f
Kirby <jkirby3286@gmail.com>
2024-05-05 12:46:51 -0400

    Tree Level widths solutions

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -10,7 +10,49 @@
 // |       |
 // 4       5
 // Answer: [1, 3, 2]
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const widths = [0];
+    let level = 0;
 
-function levelWidth(root) {}
+    while(arr.length > 1) {
+        const node = arr.shift();
+
+        if(node === 's') {
+            arr.push('s');
+            level++;
+            widths.push(0);
+            continue;
+        }
+
+        arr.push(...node.children);
+        widths[level]++;
+    }
+
+    return widths;
+}
+
+// Initial solution based on Grider explanation
+// function levelWidth(root) {
+//     const arr = [root, 's'];
+//     const widths = [0];
+//     let level = 0;
+//     while(arr.length > 1) {
+//         const node = arr.shift();
+
+//         if(node === 's') {    
+//             arr.push('s');
+//             level++;
+//             widths.push(0);
+//             continue;
+//         }
+
+//         arr.push(...node.children);
+
+//         widths[level]++;
+//     }
+
+//     return widths;
+// }
 
 module.exports = levelWidth;
