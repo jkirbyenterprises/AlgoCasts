f85b843c0cb9f5ea9ad3fb6f266209c3d6ba7b11
Kirby <jkirby3286@gmail.com>
2024-05-05 13:06:36 -0400

    BST Solution

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,79 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(value) {
+        this.data = value;
+        this.left = null;
+        this.right = null;
+    }
+    insert(value) {
+        if(value < this.data) {
+            if(this.left === null) {
+                this.left = new Node(value);
+            } else {
+                this.left.insert(value);
+            }
+        }
+
+        if(value > this.data) {
+            if(this.right === null) {
+                this.right = new Node(value);
+            } else {
+                this.right.insert(value);
+            }
+        }
+    }
+    contains(value) {
+        if(value === this.data) {
+            return this;
+        }
+        if(this.left && value < this.data) {
+            return this.left.contains(value);
+        }
+        if(this.right && value > this.data) {
+            return this.right.contains(value);
+        }
+        return null;
+    }
+}
+
+// My initial
+// class Node {
+//     constructor(value) {
+//         this.data = value;
+//         this.left = null;
+//         this.right = null;
+//     }
+//     insert(value) {
+//         if(value < this.data) {
+//             if(this.left === null) {
+//                 this.left = new Node(value);
+//             } else {
+//                 this.left.insert(value);
+//             }
+//         }
+
+//         if(value > this.data) {
+//             if(this.right === null) {
+//                 this.right = new Node(value);
+//             } else {
+//                 this.right.insert(value);
+//             }
+//         }
+//     }
+//     contains(value) {
+//         if(value === this.data) {
+//             return this;
+//         }
+//         if(this.left && value < this.data) {
+//             return this.left.contains(value);
+//         }
+//         if(this.right && value > this.data) {
+//             return this.right.contains(value);
+//         }
+//         return null;
+//     }
+// }
 
 module.exports = Node;
