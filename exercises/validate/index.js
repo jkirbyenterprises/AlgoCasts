388940e66f67f7d38b82fc15cb056ac5a3f64847
Kirby <jkirby3286@gmail.com>
2024-05-05 14:17:03 -0400

    Validate BST solutions

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,26 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+    const isNodeInvalid = (min !== null && node.data < min) || (max !== null && node.data > max);
+    if(isNodeInvalid) { return false; }
+
+    const isLeftInvalid = (node.left !== null && !validate(node.left, min, node.data));
+    if(isLeftInvalid) { return false; }
+
+    const isRightInvalid = (node.right !== null && !validate(node.right, node.data, max));
+    if(isRightInvalid) { return false; }
+
+    return true;
+}
+
+// My initial
+// function validate(node, min = null, max = null) {
+//     const isNodeValid = (min === null || node.data > min) && (max === null || node.data < max);
+//     const isLeftValid = (node.left === null) || validate(node.left, min, node.data);
+//     const isRightValid = (node.right === null) || validate(node.right, node.data, max);
+    
+//     return isNodeValid && isLeftValid && isRightValid;
+// }
 
 module.exports = validate;
