a0d45de45c46ccd28c8d77df005b24f76edd7ce8
Kirby <jkirby3286@gmail.com>
2024-05-07 23:49:00 -0400

    Fib linear solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,10 +9,18 @@
 //   fib(4) === 3
 
 function fib(n) {
-    if (n<2) {
-        return n;
+    let results = [0,1];
+    for(let index = 2; index <= n; index++) {
+        results.push(results[index-1]+results[index-2]);
     }
-    return fib(n-1) + fib(n-2);
+    return results[results.length-1];
 }
+
+// function fib(n) {
+//     if (n<2) {
+//         return n;
+//     }
+//     return fib(n-1) + fib(n-2);
+// }
 
 module.exports = fib;
