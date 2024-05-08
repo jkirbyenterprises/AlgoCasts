fba8ec0ad6b3be3630199912d2989acc05d92b94
Kirby <jkirby3286@gmail.com>
2024-05-08 00:05:35 -0400

    Fib memoized solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,14 +8,66 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
-    let results = [0,1];
-    for(let index = 2; index <= n; index++) {
-        results.push(results[index-1]+results[index-2]);
+// memoized
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if(cache[args]) {
+            return cache[args];
+        }
+        cache[args] = fn.apply(this, args);
+        return cache[args];
     }
-    return results[results.length-1];
 }
 
+const fib = memoize(slowFib);
+
+function slowFib(n) {
+    if (n<2) {
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+// function fib(n) {
+//     if(cachedResults[n]) {
+//         return cachedResults[n];
+//     }
+
+//     if (n<2) {
+//         return n;
+//     }
+
+//     cachedResults[n] = fib(n-1) + fib(n-2);
+//     return cachedResults[n];
+// }
+
+// using cache to improve exponential runtime
+// let cachedResults = {};
+// function fib(n) {
+    
+//     if(cachedResults[n]) {
+//         return cachedResults[n];
+//     }
+
+//     if (n<2) {
+//         return n;
+//     }
+
+//     cachedResults[n] = fib(n-1) + fib(n-2);
+//     return cachedResults[n];
+// }
+
+// Linear Runtime
+// function fib(n) {
+//     let results = [0,1];
+//     for(let index = 2; index <= n; index++) {
+//         results.push(results[index-1]+results[index-2]);
+//     }
+//     return results[results.length-1];
+// }
+
+// Slow recursive solution (exponential runtime)
 // function fib(n) {
 //     if (n<2) {
 //         return n;
