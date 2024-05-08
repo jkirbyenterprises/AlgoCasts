a13f3ba4e001677801e0be16b5d96d4d076bb085
Kirby <jkirby3286@gmail.com>
2024-05-07 21:43:48 -0400

    Events Solutions

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,101 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if(this.events[eventName]) {
+      this.events[eventName].push(callback); 
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if(!this.events[eventName]) {
+      return;
+    }
+
+    for(let callback of this.events[eventName]) {
+      callback();
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
+
+// Using object and array of callbacks
+// class Events {
+//   constructor() {
+//     this.events = {};
+//   }
+//   // Register an event handler
+//   on(eventName, callback) {
+//     if(!this.events[eventName]) {
+//       this.events[eventName] = [];
+//     }
+    
+//     this.events[eventName].push(callback);
+//   }
+
+//   // Trigger all callbacks associated
+//   // with a given eventName
+//   trigger(eventName) {
+//     if(!this.events[eventName]) {
+//       return;
+//     }
+
+//     this.events[eventName].forEach((callback) => {
+//       callback();
+//     });
+//   }
+
+//   // Remove all event handlers associated
+//   // with the given eventName
+//   off(eventName) {
+//     delete this.events[eventName];
+//   }
+// }
+
+// My initial solution
+// class Events {
+//   // Register an event handler
+//   on(eventName, callback) {
+//     if(!this.events) {
+//       this.events = [];
+//     }
+
+//     this.events.push({
+//       eventName,
+//       callback
+//     });
+//   }
+
+//   // Trigger all callbacks associated
+//   // with a given eventName
+//   trigger(eventName) {
+//     this.events.map((event) => {
+//       if(event.eventName === eventName) {
+//         event.callback();
+//       }
+//     })
+//   }
+
+//   // Remove all event handlers associated
+//   // with the given eventName
+//   off(eventName) {
+//     this.events = this.events.filter((event) => (
+//       event.eventName !== eventName
+//     ));
+//   }
+// }
 
 module.exports = Events;
